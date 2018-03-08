2a374395bc45c5610ea6e562a6cf27e55ad8c23d
Me <engineer.ahmed2018@gmail.com>
2018-03-08 15:27:48 +0200

    Yaaa

--- app/src/main/java/com/a7amedo/ahmed/helprequest/SendRequest.java
+++ app/src/main/java/com/a7amedo/ahmed/helprequest/SendRequest.java
@@ -3,30 +3,34 @@ package com.a7amedo.ahmed.helprequest;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SendRequest extends AppCompatActivity {
     AlertDialog alertDialog;
-    private FusedLocationProviderClient mFusedLocationClient;
     Context context;
-
+    LocationManager locationManager;
+    double lont , lat ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         context = this;
+        alertDialog = new AlertDialog.Builder(context).create();
         super.onCreate(savedInstanceState);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         setContentView(R.layout.activity_send_request);
         Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
@@ -36,7 +40,91 @@ public class SendRequest extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    lat = latitude;
+                    lont = longitude;
+                    LatLng latLng = new LatLng(latitude,longitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addresses = geocoder.getFromLocation(latitude,longitude,1);
+                        String str = addresses.get(0).getLocality()+",";
+                        str+= addresses.get(0).getCountryName();
+                        alertDialog.setMessage(latitude+" "+longitude);
+                        alertDialog.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
 
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }else if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    lat = latitude;
+                    lont = longitude;
+                    LatLng latLng = new LatLng(latitude,longitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addresses = geocoder.getFromLocation(latitude,longitude,1);
+                        String str = addresses.get(0).getLocality()+",";
+                        str+= addresses.get(0).getCountryName();
+                        alertDialog.setMessage(latitude+" "+longitude);
+                        alertDialog.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
     }
 
     public void SendRequest(View view) {
@@ -49,37 +137,11 @@ public class SendRequest extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            alertDialog = new AlertDialog.Builder(context).create();
-            alertDialog.setTitle("Status");
-            alertDialog.setMessage("False");
+            alertDialog.setMessage(lat+" "+lont);
             alertDialog.show();
             return;
         }
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        double Longtiude, Lati;
-                        if (location != null) {
-                            Longtiude = location.getLongitude();
-                            Lati = location.getLatitude();
-                            Spinner mySpinner = (Spinner) findViewById(R.id.planets_spinner);
-                            String text = mySpinner.getSelectedItem().toString();
-                            EditText editText = (EditText) findViewById(R.id.text_help);
-                            String Help_Request = editText.getText().toString();
-                            alertDialog = new AlertDialog.Builder(context).create();
-                            alertDialog.setTitle("Status");
-                            alertDialog.setMessage(Longtiude + " " + Lati);
-                            alertDialog.show();
 
-                        } else {
-                            alertDialog = new AlertDialog.Builder(context).create();
-                            alertDialog.setTitle("Status");
-                            alertDialog.setMessage("Null error");
-                            alertDialog.show();
-                        }
-                    }
-                });
 
     }
 }
