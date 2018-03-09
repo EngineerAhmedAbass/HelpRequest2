35ce4d9be94c6acf867f76f198da8825602b3a18
ahmed <ahmedgf100@gmail.com>
2018-03-09 19:14:42 +0200

    Abass

--- app/src/main/java/com/a7amedo/ahmed/helprequest/SendRequest.java
+++ app/src/main/java/com/a7amedo/ahmed/helprequest/SendRequest.java
@@ -1,21 +1,18 @@
 package com.a7amedo.ahmed.helprequest;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.content.pm.PermissionGroupInfo;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -26,7 +23,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -39,115 +35,59 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStates;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.maps.model.LatLng;
 import com.karan.churi.PermissionManager.PermissionManager;
 
 import java.io.IOException;
 import java.util.List;
 
 public class SendRequest extends AppCompatActivity {
-        AlertDialog alertDialog;
-        Context context;
-        LocationManager locationManager;
-        double lont , lat ;
-        PermissionManager permissionManager ;
-        private static final int REQUEST_CHECK_SETTINGS = 0x1;
-        private static GoogleApiClient mGoogleApiClient;
-        private static final int ACCESS_FINE_LOCATION_INTENT_ID = 3;
-        private static final String BROADCAST_ACTION = "android.location.PROVIDERS_CHANGED";
+    AlertDialog alertDialog;
+    Context context;
+    LocationManager locationManager;
+    double lont, lat;
+    PermissionManager permissionManager;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static GoogleApiClient mGoogleApiClient;
+    private static final int ACCESS_FINE_LOCATION_INTENT_ID = 3;
+    private static final String BROADCAST_ACTION = "android.location.PROVIDERS_CHANGED";
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            context = this;
-            alertDialog = new AlertDialog.Builder(context).create();
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_send_request);
-            initGoogleAPIClient();//Init Google API Client
-            checkPermissions();//Check Permission
-            Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
-    // Create an ArrayAdapter using the string array and a default spinner layout
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                    R.array.planets_array, android.R.layout.simple_spinner_item);
-    // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
-            spinner.setAdapter(adapter);
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            permissionManager = new PermissionManager() {};
-            permissionManager.checkAndRequestPermissions(this);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        context = this;
+        alertDialog = new AlertDialog.Builder(context).create();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_request);
 
+        /*  Start Spinner Code */
+        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            if(locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)){
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        double latitude = location.getLatitude();
-                        double longitude = location.getLongitude();
-                        lat = latitude;
-                        lont = longitude;
-                    }
+        /*  End Spinner Code */
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        permissionManager = new PermissionManager() {
+        };
+        permissionManager.checkAndRequestPermissions(this);
 
-                    @Override
-                    public void onStatusChanged(String s, int i, Bundle bundle) {
+        initGoogleAPIClient();//Init Google API Client
+        GetLocation();
+    }
 
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String s) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String s) {
-
-                    }
-                });
-            }else if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)){
-                locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        double latitude = location.getLatitude();
-                        double longitude = location.getLongitude();
-                        lat = latitude;
-                        lont = longitude;
-                    }
-
-                    @Override
-                    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String s) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String s) {
-
-                    }
-                });
-            }
-        }
-        private void initGoogleAPIClient() {
+    private void initGoogleAPIClient() {
         //Without Google API Client Auto Location Dialog will not work
         mGoogleApiClient = new GoogleApiClient.Builder(SendRequest.this)
                 .addApi(LocationServices.API)
                 .build();
         mGoogleApiClient.connect();
-        }
-        private void checkPermissions() {
+    }
+
+    private void checkPermissions() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (ContextCompat.checkSelfPermission(SendRequest.this,
                     android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -157,9 +97,74 @@ public class SendRequest extends AppCompatActivity {
                 showSettingDialog();
         } else
             showSettingDialog();
+    }
+    public void GetLocation(){
+        Toast.makeText(SendRequest.this, "Checking Location..", Toast.LENGTH_SHORT).show();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    lat = latitude;
+                    lont = longitude;
+                }
 
-         }
-        private void requestLocationPermission() {
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+                    GetLocation();
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                    checkPermissions();
+                }
+            });
+        } else if (locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    lat = latitude;
+                    lont = longitude;
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+                    GetLocation();
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                    checkPermissions();
+                }
+            });
+        }else{
+            checkPermissions();
+        }
+    }
+    private void requestLocationPermission() {
 
         if (ActivityCompat.shouldShowRequestPermissionRationale(SendRequest.this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
             ActivityCompat.requestPermissions(SendRequest.this,
@@ -171,7 +176,8 @@ public class SendRequest extends AppCompatActivity {
                     ACCESS_FINE_LOCATION_INTENT_ID);
         }
     }
-        private void showSettingDialog() {
+
+    private void showSettingDialog() {
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);//Setting priotity of Location request to high
         locationRequest.setInterval(30 * 1000);
@@ -191,7 +197,6 @@ public class SendRequest extends AppCompatActivity {
                     case LocationSettingsStatusCodes.SUCCESS:
                         // All location settings are satisfied. The client can initialize location
                         // requests here.
-                        updateGPSStatus("GPS is Enabled in your device");
                         break;
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
                         // Location settings are not satisfied. But could be fixed by showing the user
@@ -213,71 +218,26 @@ public class SendRequest extends AppCompatActivity {
             }
         });
     }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             // Check for the integer request code originally supplied to startResolutionForResult().
             case REQUEST_CHECK_SETTINGS:
                 switch (resultCode) {
                     case RESULT_OK:
                         Log.e("Settings", "Result OK");
-                        updateGPSStatus("GPS is Enabled in your device");
+                        GetLocation();
                         //startLocationUpdates();
                         break;
                     case RESULT_CANCELED:
-                        Log.e("Settings", "Result Cancel");
-                        updateGPSStatus("GPS is Disabled in your device");
+                        Toast.makeText(SendRequest.this, "No...", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 break;
         }
     }
-        @Override
-        protected void onResume() {
-         super.onResume();
-        registerReceiver(gpsLocationReceiver, new IntentFilter(BROADCAST_ACTION));//Register broadcast receiver to check the status of GPS
-    }
-        @Override
-        protected void onDestroy() {
-        super.onDestroy();
-        //Unregister receiver on destroy
-        if (gpsLocationReceiver != null)
-            unregisterReceiver(gpsLocationReceiver);
-    }
-        private BroadcastReceiver gpsLocationReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            //If Action is Location
-            if (intent.getAction().matches(BROADCAST_ACTION)) {
-                LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-                //Check if GPS is turned ON or OFF
-                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    Log.e("About GPS", "GPS is Enabled in your device");
-                    updateGPSStatus("GPS is Enabled in your device");
-                } else {
-                    //If GPS turned OFF show Location Dialog
-                    new Handler().postDelayed(sendUpdatesToUI, 10);
-                    // showSettingDialog();
-                    updateGPSStatus("GPS is Disabled in your device");
-                    Log.e("About GPS", "GPS is Disabled in your device");
-                }
 
-            }
-        }
-    };
-
-    //Method to update GPS status text
-    private void updateGPSStatus(String status) {
-        alertDialog.setTitle("GPS Status");
-        alertDialog.setMessage(status);
-        alertDialog.show();
-    }
-    //Run on UI
-    private Runnable sendUpdatesToUI = new Runnable() {
-        public void run() {
-            showSettingDialog();
-        }
-    };
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -286,7 +246,6 @@ public class SendRequest extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     //If permission granted show location dialog if APIClient is not null
                     if (mGoogleApiClient == null) {
                         initGoogleAPIClient();
@@ -294,9 +253,7 @@ public class SendRequest extends AppCompatActivity {
                     } else
                         showSettingDialog();
 
-
                 } else {
-                    updateGPSStatus("Location Permission denied.");
                     Toast.makeText(SendRequest.this, "Location Permission denied.", Toast.LENGTH_SHORT).show();
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
@@ -305,7 +262,8 @@ public class SendRequest extends AppCompatActivity {
             }
         }
     }
-        public void SendRequest(View view) throws IOException {
+
+    public void SendRequest(View view) throws IOException {
             permissionManager.checkAndRequestPermissions(this);
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
@@ -320,23 +278,40 @@ public class SendRequest extends AppCompatActivity {
                 alertDialog.show();
                 return;
             }
-            initGoogleAPIClient();//Init Google API Client
-            checkPermissions();//Check Permission
-            Geocoder geocoder = new Geocoder(getApplicationContext());
-            List<Address> addresses = geocoder.getFromLocation(lat,lont,1);
-            String str = addresses.get(0).getLocality()+",";
-            str+= addresses.get(0).getCountryName();
-            alertDialog.setMessage(lat+" "+lont+"  "+str);
-            alertDialog.show();
-
-
+            if(isLocationServiceEnabled()){
+                if(isNetworkAvailable()){
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    List<Address> addresses = geocoder.getFromLocation(lat,lont,1);
+                    String str = addresses.get(0).getLocality()+",";
+                    str+= addresses.get(0).getCountryName();
+                    alertDialog.setMessage(lat+" "+lont+"  "+str);
+                    alertDialog.show();
+                }else{
+                    Toast.makeText(SendRequest.this, "No Internet.", Toast.LENGTH_SHORT).show();
+                     }
+            }else {
+                Toast.makeText(SendRequest.this, "Location Is Disabled.", Toast.LENGTH_SHORT).show();
+                checkPermissions();
+            }
 
         }
-        private double deg2rad(double deg) {
-            return (deg * Math.PI / 180.0);
+    public boolean isLocationServiceEnabled(){
+        boolean gps_enabled= false;
+
+        if(locationManager ==null)
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            try{
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch(Exception ex){
+            //do nothing...
         }
 
-        private double rad2deg(double rad) {
-            return (rad * 180.0 / Math.PI);
-        }
+        return gps_enabled ;
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
